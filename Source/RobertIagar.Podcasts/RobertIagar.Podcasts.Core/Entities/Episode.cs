c94fedf90fafd5a200aac8ce7ba08421197cb530
Robert Iagar <robert.iagar@gmail.com>
2015-09-09 22:12:49 +0300

    Adding entities...

---
+++ Source/RobertIagar.Podcasts/RobertIagar.Podcasts.Core/Entities/Episode.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace RobertIagar.Podcasts.Core.Entities
+{
+    public class Episode
+    {
+        public string Name { get; set; }
+        public string WebPath { get; set; }
+        public bool IsLocal { get; set; }
+        public string Author { get; set; }
+        public DateTime Published { get; set; }
+        public Uri ImageUrl { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+
+        [JsonIgnore]
+        public virtual Podcast Podcast { get; set; }
+    }
+}
---
+++ Source/RobertIagar.Podcasts/RobertIagar.Podcasts.Core/Entities/Podcast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobertIagar.Podcasts.Core.Entities
+{
+    public class Podcast
+    {
+        public string Name { get; set; }
+        public Uri FeedUrl { get; set; }
+        public Uri ImageUrl { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime DateAdded { get; set; }
+        public Guid PodcastId { get; set; }
+
+        public virtual IList<Episode> Episodes { get; set; }
+    }
+}
