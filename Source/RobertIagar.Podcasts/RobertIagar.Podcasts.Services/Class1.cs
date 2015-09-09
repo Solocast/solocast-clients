6177c9314a1d16e24a22282e7b8aea1ccc534efe
Robert Iagar <robert.iagar@gmail.com>
2015-09-09 21:25:00 +0300

    Adding Source folder

---
+++ Source/RobertIagar.Podcasts/RobertIagar.Podcasts.Services/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobertIagar.Podcasts.Services
+{
+    public class Class1
+    {
+    }
+}
