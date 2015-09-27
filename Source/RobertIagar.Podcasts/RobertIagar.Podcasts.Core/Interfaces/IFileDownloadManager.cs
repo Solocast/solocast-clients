2eaf8ada8009fad33471ba63037b05647f174852
Robert Iagar <robert.iagar@gmail.com>
2015-09-28 00:32:42 +0300

    Converted IFileDownloader to IFileDownloadManager...

---
+++ Source/RobertIagar.Podcasts/RobertIagar.Podcasts.Core/Interfaces/IFileDownloadManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+
+namespace RobertIagar.Podcasts.Core.Interfaces
+{
+    public interface IFileDownloadManager
+    {
+        Task<StorageFile> DonwloadFileAsync(
+            string appFolderName,
+            string folderName,
+            string fileName,
+            string fileUrl,
+            Action<DownloadOperation> callback,
+            Action errorCallback = null);
+        void PauseDownload(string fileUrl);
+        void ResumeDownload(string fielUrl);
+        void CancelDownload(string fileUrl);
+        void CancelAllDownloads();
+        int NumberOfDownlaods { get; }
+    }
+}
---
+++ Source/RobertIagar.Podcasts/RobertIagar.Podcasts.Services/FileDownloadManager.cs
@@ -0,0 +1,114 @@
+﻿using RobertIagar.Podcasts.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+
+namespace RobertIagar.Podcasts.Services
+{
+    public class FileDownloadManager : IFileDownloadManager
+    {
+        private Dictionary<string, DownloadOperation> downloads;
+        private Dictionary<string, CancellationTokenSource> cancellationTokenSources;
+
+        public FileDownloadManager()
+        {
+            downloads = new Dictionary<string, DownloadOperation>();
+            cancellationTokenSources = new Dictionary<string, CancellationTokenSource>();
+        }
+
+        public int NumberOfDownlaods
+        {
+            get
+            {
+                return downloads.Count;
+            }
+        }
+
+        public void CancelAllDownloads()
+        {
+            foreach (var cancellationTokenSource in cancellationTokenSources.ToList())
+            {
+                CancelDownload(cancellationTokenSource.Key);
+            }
+        }
+
+        public void CancelDownload(string fileUrl)
+        {
+            var cancellationTokenSource = cancellationTokenSources[fileUrl];
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+            cancellationTokenSources.Remove(fileUrl);
+            downloads.Remove(fileUrl);
+        }
+
+        public async Task<StorageFile> DonwloadFileAsync(
+            string appFolderName,
+            string folderName,
+            string fileName,
+            string fileUrl,
+            Action<DownloadOperation> callback,
+            Action errorCallback = null)
+        {
+            var appFolder = await KnownFolders.MusicLibrary.CreateFolderAsync(appFolderName, CreationCollisionOption.OpenIfExists);
+            var podcastFolder = await appFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+            try
+            {
+                var uri = new Uri(fileUrl);
+                var extension = uri.AbsolutePath.GetExtension();
+                var file = await podcastFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+                var backgroundDownloader = new BackgroundDownloader();
+                var downloadOperation = backgroundDownloader.CreateDownload(uri, file);
+                var progress = new Progress<DownloadOperation>(callback);
+                downloads.Add(fileUrl, downloadOperation);
+
+                var cts = new CancellationTokenSource();
+                cancellationTokenSources.Add(fileUrl, cts);
+
+                await downloadOperation.StartAsync().AsTask(cts.Token, progress);
+
+                downloads.Remove(fileUrl);
+                cancellationTokenSources.Remove(fileUrl);
+                return file;
+            }
+            catch
+            {
+                if (errorCallback != null)
+                    errorCallback();
+                return null;
+            }
+        }
+
+        public void PauseDownload(string fileUrl)
+        {
+            var downloadOperation = downloads[fileUrl];
+            if (downloadOperation.Progress.Status == BackgroundTransferStatus.Running)
+            {
+                downloadOperation.Pause();
+            }
+        }
+
+        public void ResumeDownload(string fielUrl)
+        {
+            var downloadOperation = downloads[fielUrl];
+            if (downloadOperation.Progress.Status == BackgroundTransferStatus.PausedByApplication)
+            {
+                downloadOperation.Resume();
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string GetExtension(this string input)
+        {
+            var index = input.LastIndexOf('.');
+            return input.Substring(index);
+        }
+    }
+}
