a57ecb3a6c774776bab7489fbae4963bd10ffaed
Robert Iagar <robert.iagar@gmail.com>
2015-12-06 22:26:05 +0200

    Added DeletePodcastMessage.

---
+++ Source/RobertIagar.Podcasts/RobertIagar.Podcasts.UWP/Infrastructure/Messages/DeletePodcastMessage.cs
@@ -0,0 +1,19 @@
+﻿using RobertIagar.Podcasts.UWP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobertIagar.Podcasts.UWP.Infrastructure.Messages
+{
+    public class DeletePodcastMessage
+    {
+        public DeletePodcastMessage(PodcastViewModel podcast)
+        {
+            this.PodcastViewModel = podcast;
+        }
+
+        public PodcastViewModel PodcastViewModel { get; private set; }
+    }
+}
