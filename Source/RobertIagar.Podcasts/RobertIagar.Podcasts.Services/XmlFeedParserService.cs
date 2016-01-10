1d523e6b9f962b98b201ba9ae6d5c0c3e6169569
Robert Iagar <robert.iagar@gmail.com>
2016-01-10 18:45:05 +0200

    Adding new FeedParserService (and fix AppVeyor build)

---
+++ Source/RobertIagar.Podcasts/RobertIagar.Podcasts.Services/XmlFeedParserService.cs
@@ -0,0 +1,19 @@
+﻿using RobertIagar.Podcasts.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobertIagar.Podcasts.Core.Contracts;
+using System.Net.Http;
+
+namespace RobertIagar.Podcasts.Services
+{
+    public class XmlFeedParserService : IFeedParaseService
+    {
+        public Task<Podcast> GetPodcastAsync(string feedUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
