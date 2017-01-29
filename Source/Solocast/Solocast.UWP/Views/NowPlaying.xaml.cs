74e704e8648d774b45aeb9c10181010c1bf5d5f9
Robert Iagar <robert.iagar@gmail.com>
2017-01-29 13:13:12 +0200

    Revert "Updates package version"
    This reverts commit 72058f5c5a642eea0e9e241a451a066c64e64ac9.

--- Source/Solocast/Solocast.UWP/Views/NowPlaying.xaml.cs
+++ Source/Solocast/Solocast.UWP/Views/NowPlaying.xaml.cs
@@ -6,9 +6,9 @@ namespace Solocast.UWP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class NowPlayingPage : Page
+    public sealed partial class NowPlaying : Page
     {
-        public NowPlayingPage()
+        public NowPlaying()
         {
             this.InitializeComponent();
         }
